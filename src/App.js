82b851824133192778a3baacd6192124639385d3
Ahmet Aktaş <mail.ahmetaktas@gmail.com>
2021-11-25 00:25:21 +0300

    proje gönderildi

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.scss'
+
+import Header from './components/Header'
+import HeroSection from './components/HeroSection'
+import Categories from './components/Categories'
+import Campaings from './components/Campaings'
+import Favorites from './components/Favorites'
+import MobileApp from './components/MobileApp'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
+import { useWindowWidth } from '@react-hook/window-size'
+
+function App() {
+  const windowWidth = useWindowWidth()
+
+  return (
+    <div>
+      <Header />
+      {windowWidth < 768 && <Campaings />}
+      <HeroSection />
+      <Categories />
+      {windowWidth > 768 && <Campaings />}
+      <Favorites />
+      <MobileApp />
+      <Cards />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
